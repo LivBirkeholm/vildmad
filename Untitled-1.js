36084ff6d9845e860cd47bd4cb7983458b43d06e
Pemo0001 <pemo0001@stud.kea.dk>
2024-02-18 15:00:27 +0100

    jonas hjælp til database

---
+++ Untitled-1.js
@@ -0,0 +1,7 @@
+data = {
+  front: "pigsvamp.webp",
+  back: "https://webdodkxfjdfgh.mnsdghkjlfgjhklj",
+};
+
+document.querySelector("img.front").src = "svampe/" + data.front;
+document.querySelector("img.back").src = data.back;
